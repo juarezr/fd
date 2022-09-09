66884aa8e6f9e3d5d8877e34cd8659b02cdfae2b
Juarez Rudsatz <juarez.rudsatz.ter@estacio.br>
2022-09-09 13:47:41 -0300

    Store captured groups for regex placeholder

--- src/dir_entry.rs
+++ src/dir_entry.rs
@@ -1,9 +1,14 @@
+use std::ffi::OsStr;
 use std::{
     fs::{FileType, Metadata},
     path::{Path, PathBuf},
 };
+use std::borrow::Cow;
 
 use once_cell::unsync::OnceCell;
+use regex::bytes::Regex;
+
+use crate::filesystem;
 
 enum DirEntryInner {
     Normal(ignore::DirEntry),
@@ -13,6 +18,7 @@ enum DirEntryInner {
 pub struct DirEntry {
     inner: DirEntryInner,
     metadata: OnceCell<Option<Metadata>>,
+    matches: Vec<String>,
 }
 
 impl DirEntry {
@@ -21,6 +27,7 @@ impl DirEntry {
         Self {
             inner: DirEntryInner::Normal(e),
             metadata: OnceCell::new(),
+            matches: Vec::new(),
         }
     }
 
@@ -28,6 +35,7 @@ impl DirEntry {
         Self {
             inner: DirEntryInner::BrokenSymlink(path),
             metadata: OnceCell::new(),
+            matches: Vec::new(),
         }
     }
 
@@ -36,6 +44,10 @@ impl DirEntry {
             DirEntryInner::Normal(e) => e.path(),
             DirEntryInner::BrokenSymlink(pathbuf) => pathbuf.as_path(),
         }
+    }
+
+    pub fn matches(&self) -> &Vec<String> {
+        &&self.matches
     }
 
     pub fn into_path(self) -> PathBuf {
@@ -66,6 +78,47 @@ impl DirEntry {
             DirEntryInner::Normal(e) => Some(e.depth()),
             DirEntryInner::BrokenSymlink(_) => None,
         }
+    }
+
+    pub fn is_match(&mut self, pattern: &Regex, search_full_path: bool) -> bool {
+        let search_str = self.get_search_str(search_full_path);
+        let search_res = filesystem::osstr_to_bytes(search_str.as_ref());
+
+        let mut parts:Vec<String> = Vec::new();
+        for matched in pattern.captures_iter(&search_res) {
+            for (j, group) in matched.iter().enumerate() {
+                if j > 0 {
+                    if let Some(value) = group {
+                        let cap = value.as_bytes();
+                        let text = std::str::from_utf8(cap).unwrap();
+                        let part = text.to_string();
+                        parts.push(part);
+                    }
+                }
+            }
+        }
+        self.matches = parts;
+        self.matches.len() > 0
+    }
+
+    fn get_search_str(&self, search_full_path: bool) -> Cow<OsStr> {
+        let entry_path = self.path();
+
+        let search_str: Cow<OsStr> = if search_full_path {
+            let path_abs_buf = filesystem::path_absolute_form(entry_path)
+                .expect("Retrieving absolute path succeeds");
+            Cow::Owned(path_abs_buf.as_os_str().to_os_string())
+        } else {
+            match entry_path.file_name() {
+                Some(filename) => Cow::Borrowed(filename),
+                None => unreachable!(
+                    "Encountered file system entry without a file name. This should only \
+                        happen for paths like 'foo/bar/..' or '/' which are not supposed to \
+                        appear in a file system traversal."
+                ),
+            }
+        };
+        search_str
     }
 }
 
